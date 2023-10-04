48c0be390a0e81925d43f3c9c9de3e2339b80093
Abhishek Verma <ak8888780@gmail.com>
2023-10-05 00:43:42 +0530

    checking the index of the string

---
+++ project1/src/com/abhi/CaseCheck.java
@@ -0,0 +1,14 @@
+package com.abhi;
+
+import java.util.Scanner; //Scanner class import for taking input
+
+public class CaseCheck{ //class name should be the same as the file name
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String word = "helloabhii"; //String - data type and word - variable
+        System.out.println(word.charAt(0)); //print word
+        //at index 0th - h
+    }
+}
+
