742a4c675de66e0b60d5afd86db4c5f1e1a63f1f
Abhishek Verma <ak8888780@gmail.com>
2023-10-05 01:00:12 +0530

    CaseCheck whether - upper or lower

--- project1/src/com/abhi/CaseCheck.java
+++ project1/src/com/abhi/CaseCheck.java
@@ -9,6 +9,14 @@ public class CaseCheck{ //class name should be the same as the file name
         String word = "helloabhii"; //String - data type and word - variable
         System.out.println(word.charAt(0)); //print word
         //at index 0th - h
+        System.out.print("Enter the Character : ");
+        char ch = in.next().trim().charAt(0); // input the charcter by this line // trim means removes the extra space
+
+        if(ch >= 'a' && ch <= 'z'  ){ //&& means both conditions should be  true
+            System.out.println("LowerCase");
+        }else{
+            System.out.println("UpperCase"); //if 'if' condition doesn't execute then else condition executes
+        }
     }
 }
 
