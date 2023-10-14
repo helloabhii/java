e8be4ff90bc51532bc3fda1b166f4969b86a4ada
Abhishek Verma <ak8888780@gmail.com>
2023-10-14 16:54:16 +0530

    01 question 2 solve

--- qs/01/untitled/src/flowofprogram.java
+++ qs/01/untitled/src/flowofprogram.java
@@ -1,18 +1,36 @@
 import java.util.Scanner;
 public class flowofprogram {
     public static void main(String[] args) {
-
-//Ques 1-> Input a year and find whether it is a leap year or not ?
         Scanner in = new Scanner(System.in);
+
+////////////
+
+        System.out.println("Ques 1-> Input a year and find whether it is a leap year or not ?");
+
         System.out.print("Enter the year : ");
         int year = in.nextInt();
+
         if(year % 4 == 0){
             System.out.println("This is leap year : "+year);
-        }else{
+        } else{
             System.out.println("This is not a leap year");
         }
 
-// Ques 2->
+////////////
+
+        System.out.println("Ques 2-> Take input of two numbers and print the sum of both ?");
+
+        System.out.print("Enter the First Number : ");
+        int num_1 = in.nextInt();
+        System.out.print("Enter the Second Number : ");
+        int num_2 = in.nextInt();
+
+        int sum = num_1 + num_2 ;
+
+        System.out.println("Sum Of Two Numbers : "+ sum);
+
+////////////
         
+
     }
 }
