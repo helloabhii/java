597bcc88448e1d991b09edaef2ec78900b453175
Abhishek Verma <ak8888780@gmail.com>
2023-10-14 12:23:10 +0530

    intro to arrays

---
+++ Arrays/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // arrays collection of data types
+        //syntax
+        //datatype[]
+        //all the data type in the arrya should be same
+        int[] arr; //Declaration of array. ros is getting defined in the stack
+        arr =  new int[5]; //initializing of array actual here object is being created
+        System.out.println(arr[1]);
+
+        String[] ars = new String[4];
+        System.out.println(ars[0]);
+    }
+
+
+
+}
